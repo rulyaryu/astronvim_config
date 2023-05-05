9e3b7ed40ed199a96ae85a66061eb495632c5f37
Ruslan <rulyaryu@gmail.com>
2023-05-06 01:53:08 +0300

    -add laravel plug and blade formatter

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,9 +1,16 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  -- auto_install = true,
+
+  highlight = {
+    enable = false,
+    disable = { "blade.php" },
+  },
   opts = function(_, opts)
     -- add more things to the ensure_installed table protecting against community packs modifying it
     opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {
-      -- "lua"
+      "lua",
+      "php",
     })
   end,
 }
